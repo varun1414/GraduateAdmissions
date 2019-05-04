2c062975be8f6de605298c6ab888794a55452641
varun1414 <naik.varun99@gmail.com>
2019-05-04 21:58:11 +0530

    PLOTTING LEFT

--- TRAINING.m
+++ TRAINING.m
@@ -4,11 +4,18 @@ m=mean(X);
 s=std(X);
 
 y=X(:,9);
-for i=1:8
+for i=2:8
 xm=m(i);
 xs=s(i);
 x(i,:)=X(:,i)';
 x(i,:)=(x(i,:)-xm)./xs;
 plot(x(i,:),y,'+');
 hold on;
+pause;
+
 end;
+
+%Predicting Parameters
+iterations=10;
+alpha=0.00000001; theta=[1;1;1;1;1;1;1];
+theta=GradientDescent(alpha,iterations,X,y,theta);
