74ef52030c93f89b62104b8b87f0f003a1115290
varun1414 <naik.varun99@gmail.com>
2019-05-05 17:04:58 +0530

    TESTED

--- CostFunction.m
+++ CostFunction.m
@@ -1,7 +1,7 @@
-function [J]=CostFunction(X,y,m,theta)
-for i=1:7
-h=(X(:,2:8)*theta)-y;
-plot(h);
+function J=CostFunction(x,y,m,theta)
+
+h=(x(:,1:7)*theta)-y;
+%plot(h);
 s=sum(h.^2);
 J=(1/(2*m))*s;
-J
+
