68aa731f001f9d96eb8b0cd1fdf43026d48ceb06
varun1414 <naik.varun99@gmail.com>
2019-05-05 17:05:39 +0530

    Parameters (Theta) obtained

---
+++ GradientDescent.m
@@ -0,0 +1,21 @@
+function theta=GradientDescent(alpha,iterations,x,y,theta)
+m=size(x,1);
+
+for i=1:iterations 
+	J(i)=CostFunction(x,y,m,theta);
+	JA=J(i);
+	h=(x(:,1:7)*theta)-y;
+	for j=1:7
+		s=sum(h.*(x(:,j)));
+		theta(j,:)=theta(j,:)-alpha*((1/m)*s);
+	end;
+
+end;
+	
+		
+	
+	
+	t=[-1:1];
+	%plot(x*theta);
+
+	
