2d2bf4beff7461ca3a7a6abd066e0f9b7e0c4571
varun1414 <naik.varun99@gmail.com>
2019-05-06 21:54:11 +0530

    Debugged gradient descent error

--- GradientDescent.m
+++ GradientDescent.m
@@ -1,21 +1,30 @@
-function theta=GradientDescent(alpha,x,y,theta)
+function theta=GradientDescent(alpha,iterations,x,y,theta)
 m=size(x,1);
-
-while JA>0.1 
-	J(i)=CostFunction(x,y,m,theta);
-	JA=J(i);
-	h=(x(:,1:7)*theta)-y;
-	for j=1:7
+JA=1;i=1;
+for i=1:iterations 
+	J(i)=CostFunction(x,y,m,theta)
+	%JA=J(i)
+	
+	
+	h=(x(:,1:8)*theta)-y;
+	
+	for j=2:8
+		
+	
+		
+		
+		
 		s=sum(h.*(x(:,j)));
 		theta(j,:)=theta(j,:)-alpha*((1/m)*s);
-	end;
-
-end;
 	
+end ;
+theta(1,:)=theta(1,:)-alpha*((1/m)*sum(h));
+end;
+	plot(J);
 		
 	
 	
-	t=[-1:1];
-	plot(x*theta);
+	%t=[-1:1];
+	%plot(x*theta);
 	
 	
--- TRAINING.m
+++ TRAINING.m
@@ -9,34 +9,34 @@ m1=mean(y);
 s1=std(y);
 y=(y-m1)./s1;
 for k=1:7
-	x(:,k)=X(:,k+1).^k;
+	x(:,k)=X(:,k+1);
 	end;
-	
+	x=[ones(400,1),x];
 	
 m=mean(x);
 s=std(x);
 
-for i=1:7
+for i=1:8
 xm=m(i);
 xs=s(i);
 
 x(:,i)=(x(:,i)-xm)./xs;
 
- fprintf("\n\nPlotting Features\n\n");
- scatter([2:401],x(:,i),'+');
+ %fprintf("\n\nPlotting Features\n\n");
+ %scatter([2:401],x(:,i),'+');
  
 
  hold on;
  
 
 end;
-plot([2:401],y);
+%plot([2:401],y);
 
 
 %Predicting Parameters
-%iterations=1500;
-alpha=0.3; theta=[0;0;0;0;0;0;0];
-theta=GradientDescent(alpha,x,y,theta)
+iterations=10;
+alpha=0.3; theta=[0;0;0;0;0;0;0;0];
+theta=GradientDescent(alpha,iterations,x,y,theta)
 fprintf("\n\nTesting ");
 l=testing(m,s,s1,m1,theta);
 
